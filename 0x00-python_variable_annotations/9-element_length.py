bc8431ddee5f31d659f94c419df1dc304270b21b
skfada <adamusolomon44@gmail.com>
2024-03-14 14:47:50 +0100

    9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Annotate the below function’s parameters and return
+values with the appropriate types
+"""
+
+
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> \
+        typing.List[typing.Tuple[typing.Sequence, int]]:
+    """Returns a list of tuples"""
+    return [(i, len(i)) for i in lst]
